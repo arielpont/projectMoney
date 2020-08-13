d28a316391411ff9a280941acf6f622323dbb425
Ariel Pont <arielpont@hotmail.com>
2020-08-13 15:57:34 -0300

    Add some new functions on functions.py and add colors to the prints.

--- app.py
+++ app.py
@@ -1,27 +1,65 @@
 # 1st python libraries
 # import datetime as dt
 
-# 2nd custom libraries
-from modules.functions import clear
-
-# 3ed third party libraries
-# import matplotlib.pyplot as plt
-# from matplotlib import style
+# 2nd third party libraries
 import pandas as pd
-# import pandas_datareader.data as web
 
+# 3ed custom libraries
+from modules.functions import clear, confirm, delete_file, print_error
+from modules.bcolor import Bcolors
 
 # run when this file is executed as main (python app.py)
 if __name__ == "__main__":
     clear()
 
+    DATABASE_PATH = "dist/database.csv"
+
+    name = input("Name: ")
+    surname = input("Hola meté tu apellido: ")
+    email = input("Email: ")
+
+    data = {
+        "Name": [name, "Nacho", "Franco", "Leo", "Ari"],
+        "Surname": [surname, "Spera", "Venini", "Banche", "Pont"],
+        "Email": [email, "nacho@gmail.com", "franco@gmail.com", "banche@gmail.com", "ari@gmail.com"]
+    }
+
+    # Create a dataframe
+    df = pd.DataFrame(data, columns = ["Name", "Surname", "Email"])
+
+    # Create the .csv file
+    try:
+        df.to_csv(DATABASE_PATH)
+    except PermissionError:
+        print_error("El archivo está abierto en otro programa, por favor cerrarlo y volver a intentar.")
+
+    # Update the .csv file
+    new_data = {'Email': ["arielpont41@gmail.com"]}
+    new_df = pd.DataFrame(new_data, index = [4])
+    
+    try:
+        df.update(new_df)
+    except PermissionError:
+        print_error("El archivo está abierto en otro programa, por favor cerrarlo y volver a intentar.")
+
+    # Read and print the new dataframe updated
+    clear()
+    df2 =  pd.read_csv(DATABASE_PATH)
+    print(f"{Bcolors.HEADER}{df2}{Bcolors.ENDC}")
+
+    # Delete
+    if confirm("¿Desea borrar la base de datos?"):
+        delete_file(DATABASE_PATH)
+    else:
+        print(f"{Bcolors.OKGREEN}La base de datos se ha conservado{Bcolors.ENDC}")
+
     # menu options
-    options = (
+    options = [
         "Show values",
         "Show Top 10 values",
         # "Exit" must be always the last option
         "Exit"
-    )
+    ]
 
     # menu
     while True:
--- modules/functions.py
+++ modules/functions.py
@@ -1,12 +1,51 @@
 # 1st python libraries
 import os
-# 2nd custom libraries
-# 3ed third party libraries
+# 2nd third party libraries
+# 3ed custom libraries
+from modules.bcolor import Bcolors
 
-def clear(): 
+def clear():
+    """ Clear the console on Windows, MacOS and Linux. """
+
     # windows 
     if os.name == "nt": 
         _ = os.system("cls") 
     # mac and linux
     else: 
         _ = os.system("clear")
+
+def confirm(msg):
+    """ Return True if user write 'yes' or 'y' and False 'no' or 'n' (not sensible). """
+
+    while True:
+        userInput = str(input(f"{msg}\n[YES/NO]: {Bcolors.OKGREEN}"))
+        print(f"{Bcolors.ENDC}")
+
+        if userInput.lower() in ('y', 'yes'):
+            return True
+        elif userInput.lower() in ('n', 'no'):
+            return False
+        else:
+            print_error("Ustede no ingresó una opción válida, por favor vuelva a intentarlo.")
+            continue
+
+def delete_file(path):
+    """ Delete a file in a path. Acepts one String argument with the absolute path. """
+
+    if os.path.exists(path):
+        try:
+            os.remove(path)
+            return True
+        except:
+            print_error("No se pudo borrar el archivo por algún error desconocido.")
+    else:
+        print_error("El archivo que intenta borrar no existe.")
+    
+    return False
+
+def print_error(msg):
+    """ Print with style an error messagge. """
+
+    print(f"\n{Bcolors.CREDBG}  {msg}  {Bcolors.ENDC}")
+    input(f"{Bcolors.FAIL}-> Presiona ENTER para continuar{Bcolors.ENDC}\n")
+    clear()
